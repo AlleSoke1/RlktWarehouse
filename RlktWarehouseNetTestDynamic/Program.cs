b4a6f11efcdb19e52a74852418f3efb4bfd42dc0
haroharo <greg@acme>
2021-10-06 07:36:29 +0300

    daily

---
+++ RlktWarehouseNetTestDynamic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace RlktWarehouseNetTestDynamic
+{
+
+
+    internal class Program
+    {
+#if X64
+        const string LibraryName = "RlktWarehouseUpdaterLib_x64.dll";
+#else
+        const string LibraryName = "RlktWarehouseUpdaterLib_x86.dll"; 
+#endif
+
+        [DllImport(LibraryName)]
+        static extern int Initialize(int nAppId, int nCurVersion, [MarshalAs(UnmanagedType.LPStr)] string pszServerAddr, int nPort);
+
+        [DllImport(LibraryName)]
+        static extern int HasNewerVersion();
+
+        static void Main(string[] args)
+        {
+            Initialize(0, 1, "127.0.0.1", 9666);
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
